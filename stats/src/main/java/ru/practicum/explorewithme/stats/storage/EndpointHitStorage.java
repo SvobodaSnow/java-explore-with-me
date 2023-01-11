3578e19ce83de5b0343cea15001f029e04aadd7f
Svoboda Snow <gfdkbrx99@yandex.ru>
2023-01-11 12:27:15 +0300

    Перед первой проверкой

--- stats/src/main/java/ru/practicum/explorewithme/stats/storage/EndpointHitStorage.java
+++ stats/src/main/java/ru/practicum/explorewithme/stats/storage/EndpointHitStorage.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public interface EndpointHitStorage extends JpaRepository<EndpointHit, Long> {
     long countByAppContainsIgnoreCase(String app);
+
     List<EndpointHit> findByUriAndTimestampBetween(
             String uri,
             LocalDateTime timestampStart,
