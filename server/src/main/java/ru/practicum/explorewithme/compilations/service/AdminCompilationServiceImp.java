86e0bbd9722785b00e26e727c98eb4a69e14c8e8
Svoboda Snow <gfdkbrx99@yandex.ru>
2023-01-12 15:34:45 +0300

    Перед первой проверкой

--- server/src/main/java/ru/practicum/explorewithme/compilations/service/AdminCompilationServiceImp.java
+++ server/src/main/java/ru/practicum/explorewithme/compilations/service/AdminCompilationServiceImp.java
@@ -2,6 +2,7 @@ package ru.practicum.explorewithme.compilations.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import ru.practicum.explorewithme.compilations.dto.CompilationDto;
 import ru.practicum.explorewithme.compilations.dto.CompilationMapper;
@@ -9,7 +10,9 @@ import ru.practicum.explorewithme.compilations.model.Compilation;
 import ru.practicum.explorewithme.compilations.storage.CompilationStorage;
 import ru.practicum.explorewithme.events.model.Event;
 import ru.practicum.explorewithme.events.storage.EventStorage;
+import ru.practicum.explorewithme.exceptions.model.ValidationException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Primary
@@ -22,23 +25,17 @@ public class AdminCompilationServiceImp implements AdminCompilationService {
 
     @Override
     public Compilation addNewCompilation(CompilationDto compilationDto) {
-//        if (compilationDto.getEvents() == null) {
-//            throw new ValidationException(
-//                    "Отсутствует список событий",
-//                    "Список событий пуст",
-//                    LocalDateTime.now()
-//            );
-//        }
-//        if (compilationDto.getTitle() == null || compilationDto.getTitle().isEmpty()) {
-//            throw new ValidationException(
-//                    "Отсутствует заголовок подборки событий",
-//                    "Заголовок событий пуст",
-//                    LocalDateTime.now()
-//            );
-//        }
-        List<Event> events = eventStorage.findAllById(compilationDto.getEvents());
-        Compilation compilation = CompilationMapper.toCompilation(compilationDto, events);
-        return compilationStorage.save(compilation);
+        try {
+            List<Event> events = eventStorage.findAllById(compilationDto.getEvents());
+            Compilation compilation = CompilationMapper.toCompilation(compilationDto, events);
+            return compilationStorage.save(compilation);
+        } catch (DataIntegrityViolationException e) {
+            throw new ValidationException(
+                    "Отсутствует список событий",
+                    "Список событий пуст",
+                    LocalDateTime.now()
+            );
+        }
     }
 
     @Override
