0fc0d0c63162ea19fe5dbe7bf32342646137963b
Svoboda Snow <gfdkbrx99@yandex.ru>
2023-01-12 19:43:39 +0300

    Перед первой проверкой

--- server/src/main/java/ru/practicum/explorewithme/client/model/EndpointHit.java
+++ server/src/main/java/ru/practicum/explorewithme/client/model/EndpointHit.java
@@ -1,5 +1,6 @@
 package ru.practicum.explorewithme.client.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -16,5 +17,6 @@ public class EndpointHit {
     private String app;
     private String uri;
     private String ip;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
--- stats/src/main/java/ru/practicum/explorewithme/stats/storage/EndpointHitStorage.java
+++ stats/src/main/java/ru/practicum/explorewithme/stats/storage/EndpointHitStorage.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.explorewithme.stats.model.EndpointHit;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
 
 public interface EndpointHitStorage extends JpaRepository<EndpointHit, Long> {
